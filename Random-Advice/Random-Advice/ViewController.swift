c133506b413d74e2b15d617139dac3b04515dbcd
CheckoutUser <checkoutuser@is-ct-a025404.local>
2017-07-01 23:23:14 -0700

    Initial base xcode files for a single-view application

---
+++ Random-Advice/Random-Advice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Random-Advice
+//
+//  Created by CheckoutUser on 7/1/17.
+//  Copyright © 2017 Anand_Batjargal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
