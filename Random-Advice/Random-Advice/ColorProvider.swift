1b476a3b277f57c44550a2aef0c345844b0199da
CheckoutUser <checkoutuser@is-ct-a025404.local>
2017-07-02 01:47:46 -0700

    Successfully added functionality to obtain random advice from the API

---
+++ Random-Advice/Random-Advice/ColorProvider.swift
@@ -0,0 +1,16 @@
+//
+//  ColorProvider.swift
+//  Random-Advice
+//
+//  Created by CheckoutUser on 7/2/17.
+//  Copyright © 2017 Anand_Batjargal. All rights reserved.
+//
+
+import UIKit
+
+struct ColorProvider {
+    let colors = [
+        UIColor(red: <#T##CGFloat#>, green: <#T##CGFloat#>, blue: <#T##CGFloat#>, alpha: <#T##CGFloat#>)
+    
+    ]
+}
--- Random-Advice/Random-Advice/ViewController.swift
+++ Random-Advice/Random-Advice/ViewController.swift
@@ -8,19 +8,58 @@
 
 import UIKit
 import SwiftyJSON
+import Alamofire
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var adviceLabel: UILabel!
+    let baseURL = "http://api.adviceslip.com/advice"
+    var advice:String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        Alamofire.request(baseURL).responseJSON { (responseData) -> Void in if((responseData.result.value) != nil) {
+            let swiftyJsonVar = JSON(responseData.result.value!)
+            
+            //print(swiftyJsonVar["slip"]["advice"].string!)
+            self.adviceLabel.text = swiftyJsonVar["slip"]["advice"].string!
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func getAdvice() -> () {
 
+        Alamofire.request(baseURL).responseJSON { (responseData) -> Void in if((responseData.result.value) != nil) {
+                let swiftyJsonVar = JSON(responseData.result.value!)
+                
+                //print(swiftyJsonVar["slip"]["advice"].string!)
+                self.advice = swiftyJsonVar["slip"]["advice"].string!
+            }
+        }
+    }
 
+    func randomColor() -> UIColor {
+        let red = CGFloat(arc4random_uniform(256)) / 255.0
+        let green = CGFloat(arc4random_uniform(256)) / 255.0
+        let blue = CGFloat(arc4random_uniform(256)) / 255.0
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+    }
+    
+    @IBAction func showAnotherAdvice(_ sender: Any) {
+        // Modify the adviceLabel with a random advice.
+        getAdvice()
+        adviceLabel.text = advice
+        self.view.backgroundColor = randomColor()
+    }
+    
+    
 }
 
